992cc2ce4782b986dab34e18332178984156b790
thegeek-sys <55345633+thegeek-sys@users.noreply.github.com>
2023-09-27 15:42:04 +0200

    first commit

---
+++ LAB.01/challenge01.py
@@ -0,0 +1,23 @@
+from math import *
+
+print(4+4-4-4) #0
+print((4+4)//(4+4)) #1
+print(4//4+4//4) #2
+print((4+4+4)//4) #3
+print((4+4)-int(sqrt(4))-int(sqrt(4))) #4
+print((4*4+4)//4) #5
+print(factorial(4)//4+4-4) #6
+print(4+4-4//4) #7
+print(4+4+4-4) #8
+print(4+4+4//4) #9
+print(4+4+4-int(sqrt(4))) #10
+print(factorial(4)//int(sqrt(4))-4//4) #11
+print(4*4-int(sqrt(4))-int(sqrt(4))) #12
+print(44//4+int(sqrt(4))) #13
+print(4+4+4+int(sqrt(4))) #14
+print(44//4+4) #15
+print(4*4+4-4) #16
+print(4*4+4//4) #17
+print(4*4+4-int(sqrt(4))) #18
+print(factorial(4)-4-4//4) #19
+print(4*4+int(sqrt(4))+int(sqrt(4))) #20
