0b5a96f0eb6dded76baabc33f7eaedb5512265f8
Flavio <55345633+thegeek-sys@users.noreply.github.com>
2023-11-09 11:23:04 +0100

    HW4

---
+++ FP/HW/HW4req/program01.eng.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Ajeje the librarian, recently found a hidden room
+in the Keras Library (a great place located in
+Umkansa, the largest village in the White Mountains).
+There, she discovered several books, containing
+music scores of ancient Tarahumara songs.
+So, she invited over a musician friend to have a look
+at them, and he informed her that the scores are
+written in Tarahumara notation and need to be translated
+into a notation familiar to Umkansanian musicians,
+so they can play them back.
+
+Tarahumaras used numbers instead of letters for
+writing notes:
+0 in place of A, 1 in place of B, and so on, until
+7 in place of G. Flat (b) and sharp (#) notes
+(see note 3 below, if you do not know what flat
+and sharp notes are)
+were followed by a - and a +, respectively (for example,
+0- meant flat A). Moreover, they just repeated the
+same number multiple times to represent the note's
+duration. For example, 0000 would mean that the
+A note had a length of 4, while 0-0-0-0- would mean
+that the A flat note had a length of 4.
+Pauses were written down
+as spaces; for example, twelve spaces represent
+a pause of 12. Both notes and pauses could span
+different lines of the score (e.g., starting on line
+x and continuing on line x + 1, x + 2, and so on).
+Finally, music scores were written from right
+to left and from top to bottom, and going to a new
+line did not mean anything in terms of the music score.
+Umkansanians, instead, are used to write down notes using letters,
+and each note is followed by its duration (so, the example
+above would be written as A4). Flat and sharp notes are
+followed by a b or a #, respectively (for example, A flat
+is written as Ab, so the example above would be written ad
+Ab4). Pauses are written using the letter P, followed by
+their duration, and no spaces are used at all.
+Finally, they are used to read music from left
+to right on a single row.
+
+As Ajeje knows that you are a skilled programmer, she
+provides you with a folder containing the transcription
+of all the Tarahumara songs she found, organized in
+multiple subfolders and files (one song per file).
+Also, she prepared an index file in which each row
+contains the title of a Tarahumara song (in quotes),
+followed by a space and the path of the file containing
+that song (in quotes, relative to the root folder).
+She would like to translate all the songs listed in
+the index and store them into new files, each one
+named with the title of the song it contains (.txt),
+in a folder structure matching the original one.
+Also, she would like to store in the root folder of
+the created structure, a file containing on each row
+the title of a song (in quotes) and the corresponding
+song length, separated by a space. Songs in the index
+need to be ordered in descending length and, if the
+length of some songs is the same, in ascending alphabetical
+order. The length of a song is the sum of the durations
+of all notes and pauses it is made of.
+
+Would you be able to help Ajeje out in translating
+the Tarahumara songs into Umkansanian ones?
+
+Note 0: below, you are provided with a function to
+Umkansanize the Tarahumara songs; after being executed,
+it must return a dictionary in which each key is a song
+title and the associated value is the song's duration
+
+Note 1: the songs index file index.txt
+is stored in the source_root folder
+
+Note 2: the index of the translated songs
+index.txt is in the target_root folder
+
+Note 3: flat and sharp notes are just "altered" versions
+of regular notes; for example an F# ("F sharp") is the
+altered version of an F, that is, an F note which is a
+half of a tone higher than a regular F; the same holds for
+flat notes, which are a half of a tone lower than regular notes;
+from the point of view of the homework, flat and sharp notes
+must be treated the same as regular notes (except for their notation).
+
+Note 4: to create the directory structure you can use the 'os' library functions
+(e.g. os.makedirs)
+'''
+
+import os
+
+def Umkansanize(source_root:str, target_root:str) -> dict[str,int]:
+    pass
+
+if __name__ == "__main__":
+    Umkansanize("Tarahumara", "Umkansanian")
---
+++ FP/HW/HW4req/program01.ita.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Ajeje la bibliotecaria ha recentemente trovato una stanza nascosta
+nella biblioteca di Keras (un posto fantastico situato in
+Umkansa, il villaggio più grande delle Montagne Bianche).
+Lì ha scoperto diversi libri contenenti
+spartiti musicali di antiche canzoni Tarahumara e ha,
+quindi, invitato un amico musicista a dare un'occhiata.
+Il musicista le ha detto che è una scoperta sensazionale,
+dato che gli spartiti sono scritti in notazione Tarahumara,
+una popolazione ormai estinta, ma molto famosa per 
+aver influenzato i musicisti della Sierra Nevada. Per poter
+riprodurre i brani le suggerisce di farli tradurre in una
+notazione familiare ai musicisti Umkansaniani, ultimi
+discendenti dei Tarahumara, in modo che possano riprodurli.
+
+I Tarahumara scrivevano le note usando numeri invece di lettere:
+0 al posto di A, 1 al posto di B e così via, fino a 7 al posto di G. 
+Le note bemolle (b) e diesis (#)
+(vedi la nota 3 sotto, se non sai cosa sono bemolle e diesis)
+erano seguite rispettivamente da un - e da un + 
+(ad esempio, 0- significa A bemolle). 
+Inoltre, ripetevano semplicemente lo stesso numero più volte 
+per rappresentare la durata della nota. 
+Ad esempio, 0000 significa che la nota A ha una lunghezza di 4, 
+mentre 0-0-0-0- significa che la nota A bemolle ha una lunghezza di 4.
+
+Le pause venivano scritte come spazi:
+ad esempio, dodici spazi rappresentano una pausa lunga 12. 
+Sia le note che le pause potevano estendersi su
+diverse linee della partitura (ad esempio, iniziando dalla linea
+x e continuando sulla riga x+1, x+2 e così via).
+Infine, gli spartiti musicali erano scritti da destra
+a sinistra e dall'alto verso il basso, mentre andare accapo 
+non significava nulla in termini di partitura musicale.
+
+Gli Umkansaniani, invece, sono soliti scrivere le note utilizzando lettere,
+e ogni nota è seguita dalla sua durata (quindi, l'esempio
+sopra verrebbe scritto come A4). 
+Le note bemolle e diesis sono seguite rispettivamente 
+da una 'b' o da una '#' (ad esempio, A bemolle è scritto Ab, 
+quindi l'esempio sopra verrebbe scritto ad Ab4). 
+Le pause vengono scritte utilizzando la lettera P, seguita dalla 
+loro durata e non viene utilizzato alcuno spazio.
+Infine, gli Umkansaniani sono abituati a leggere la musica da
+sinistra a destra, scritta su una singola riga.
+
+Poiché Ajeje sa che sei un abile programmatore, 
+ti fornisce una cartella contenente la trascrizione
+di tutte le canzoni di Tarahumara che ha trovato, 
+organizzate in più sottocartelle e file (un brano per file).
+Inoltre, ha preparato un file indice in cui ogni riga
+contiene il titolo di una canzone Tarahumara (tra virgolette),
+seguito da uno spazio e dal percorso del file contenente
+quella canzone (tra virgolette, relativa alla cartella principale).
+Vorrebbe tradurre tutte le canzoni elencate nell'indice e 
+salvarle in nuovi file, ciascuno denominato con il titolo 
+della canzone che contiene (con estensione .txt),
+in una struttura di cartelle corrispondente a quella originale.
+Inoltre, vorrebbe archiviare nella cartella principale della
+struttura creata un file indice contenente su ogni riga
+il titolo di una canzone (tra virgolette) e la corrispondente
+lunghezza del brano, separati da uno spazio. 
+Le canzoni nell'indice devono essere ordinate per lunghezza decrescente e, 
+se la durata di alcuni brani è la stessa, in ordine alfabetico ascendente. 
+La durata di una canzone è la somma delle durate
+di tutte le note e delle pause di cui è composta.
+
+Sarai capace di aiutare Ajeje nel tradurre le canzoni
+Tarahumara in canzoni Umkansaniane?
+
+Nota 0: di seguito viene fornita una funzione per
+Umkansanizzare le canzoni di Tarahumara; 
+dopo essere stata eseguita, deve restituire un dizionario 
+in cui ogni chiave è il titolo di una canzone
+ed il valore associato è la durata del brano.
+
+Nota 1: l'indice delle canzoni da tradurre è il file 'index.txt'
+che si trova nella directory passata nell'argomento source_root
+
+Nota 2: l'indice delle canzoni tradotte è il file 'index.txt'
+che deve essere creato nella directory passata nell'argomento target_root
+
+Nota 3: le note bemolle e diesis sono solo versioni "alterate".
+di note regolari; per esempio un A# ("A diesis") è la
+versione alterata di un A, cioè una nota A che è un
+mezzo tono più alto del A regolare; lo stesso vale per
+note bemolle, che sono mezzo tono più basse delle note normali;
+dal punto di vista dei compiti, note bemolle e diesis
+devono essere trattate allo stesso modo delle note regolari 
+(ad eccezione della loro notazione).
+
+Nota 4: Usiamo la notazione inglese delle note A B C D E F G.
+
+Nota 5: potete usare le funzioni della libreria 'os' per creare le directory necessarie
+(ad esempio os.makedirs)
+'''
+
+import os
+
+def translate_files(pretty_files: list):
+    notes = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', '6': 'G', '-': 'b', '+': '#'}
+    for file in pretty_files:
+        with open(file, mode='rt') as file:
+            pass
+
+        with open(file, mode='wt') as file:
+            pass
+
+def sanitize_txt(titles: dict) -> list:
+    pretty_files = []
+    for title, path in titles.items():
+        sanitized = []
+        print('\n\n\n\n\n')
+        with open(path, mode='rt') as song:
+            print(path)
+            song=song.readlines()
+            for i in range(len(song)):
+                sanitized.append(song[i][::-1])
+            #sanitized.append(reversed(song.readlines()[::-1]))
+            sanitized = ''.join([x.replace('\n','') for x in sanitized])
+        with open(f'{os.path.dirname(path)}/{title}.txt', mode='wt') as song:
+            song.write(sanitized)
+        pretty_files.append(f'{os.path.dirname(path)}/{title}.txt')
+    return pretty_files
+        
+        
+
+def get_titles_files() -> dict[str,str]:
+    path = ['test'+str(i).zfill(2) for i in range(1,11)]
+    titles = {}
+    for dir in path:
+        rel_path = f'{dir}/index.txt'
+        with open(rel_path, mode='rt') as file_titles:
+            file_titles = file_titles.read().split('\n')
+            for tit_file in file_titles[:-1]:
+                single = tit_file.split('" "')
+                single[0], single[1] = single[0].replace('"', ''), single[1].replace('"', '')
+                titles[single[0]] = f'{dir}/{single[1]}'
+    return titles
+        
+
+def Umkansanize(source_root:str, target_root:str) -> dict[str,int]:
+    pass
+
+if __name__ == "__main__":
+    #Umkansanize("Tarahumara", "Umkansanian")
+    print(sanitize_txt(get_titles_files()))
