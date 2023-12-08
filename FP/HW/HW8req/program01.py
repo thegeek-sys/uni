6eca26f97c0ca9d9053f6b4cb793947c46500e42
Flavio <55345633+thegeek-sys@users.noreply.github.com>
2023-12-08 20:23:29 +0100

    first working test

--- FP/HW/HW8req/program01.py
+++ FP/HW/HW8req/program01.py
@@ -43,9 +43,9 @@ NOTA: almeno una delle funzioni o metodi che realizzate deve essere ricorsiva
 NOTA: la funzione/metodo ricorsivo/o deve essere definita a livello più esterno
       altrimenti fallirete il test di ricorsione.
 '''
-import json
+#import json
 
-def translate(encrypted, original, new):
+'''def translate(encrypted, original, new):
     l = len(original)+1
     #original = list(original)
     translated = list(encrypted)
@@ -58,18 +58,73 @@ def translate(encrypted, original, new):
         i += 1
     return ''.join(translated)
 
+def recurs(encrypted_list, pharaohs_cypher):
+    for encrypted in encrypted_list:
+        for k, v in pharaohs_cypher.items():
+            l = len(k)+1
+            translated = list(encrypted)
+            #print(k)
+            i = 0
+            while i < len(translated):
+                #print(''.join(translated[i:i+l]))
+                if all(x in list(translated[i:i+l]) for x in k):
+                    translated[i:i+l] = v
+                    print(encrypted_list)
+                    return recurs([''.join(translated)]+encrypted_list, pharaohs_cypher)
+                elif (list(pharaohs_cypher.keys())[-1] == k) and (i == len(translated)-1):
+                    return encrypted_list
+                i += 1
+                '''
+
+def recurs1(encrypted_list, k, v, j, q):
+    while q < len(k):
+        l = len(k[q])+1
+        translated = list(encrypted_list[j])
+        #print(k)
+        i = 0
+        while i < len(translated):
+            #w = ''.join(translated[i:i+l])
+            #y = k[q]
+            #z = encrypted_list[j]
+            print()
+            #print(''.join(translated[i:i+l]))
+            if all(x in list(translated[i:i+l]) for x in k[q]):
+                new = translated[:]
+                new[i:i+l] = v[q]
+                if ''.join(new) not in encrypted_list:
+                    #print(encrypted_list)
+                    return recurs1(encrypted_list+[''.join(new)], k, v, j, q)
+            elif (q == len(k)-1) and (i == len(translated)-1):
+                if j+1 != len(encrypted_list):
+                    return recurs1(encrypted_list, k, v, j+1, 0)
+            i += 1
+        q += 1
+    #print(encrypted_list)
+    return encrypted_list
+    
+
 
 #def recurs():
     
 
 def pharaohs_revenge(encrypted_text : str, pharaohs_cypher : dict[str,str]) -> set[str]:
-    i = 0
+    '''i = 0
     while i < 3:
         for k, v in pharaohs_cypher.items():
             k = list(k)
             encrypted_text = translate(encrypted_text, k, v)
             print(encrypted_text)
-        i += 1
+        i += 1'''
+    k = list(pharaohs_cypher.keys())
+    v = list(pharaohs_cypher.values())
+    a = recurs1([encrypted_text], k, v, 0, 0)
+    res = min(len(ele) for ele in a)
+    short_name = [name for name in a if len(name) == res]
+    print(short_name)
+    return set(short_name)
+    
+    #a = recurs([encrypted_text], pharaohs_cypher)
+    #print(a)
 
 
 
